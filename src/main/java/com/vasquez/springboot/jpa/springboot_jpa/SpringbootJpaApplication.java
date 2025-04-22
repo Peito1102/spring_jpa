b4758fc10b8efd905d2b920c43645c34c5364d84
Peito1102 <renzovasquezch@hotmail.com>
2025-04-22 02:16:49 -0500

    desde 1 hasta 4 (jpa 09)

---
+++ src/main/java/com/vasquez/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
@@ -0,0 +1,20 @@
+package com.vasquez.springboot.jpa.springboot_jpa;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'run'");
+	}
+
+}

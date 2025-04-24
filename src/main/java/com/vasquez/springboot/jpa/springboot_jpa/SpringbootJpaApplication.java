91a298f18ec9420c4ce7100842a64d492341e5d3
Peito1102 <renzovasquezch@hotmail.com>
2025-04-24 02:46:57 -0500

    parte 5 (jpa 09)

--- src/main/java/com/vasquez/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
+++ src/main/java/com/vasquez/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
@@ -1,11 +1,20 @@
 package com.vasquez.springboot.jpa.springboot_jpa;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.vasquez.springboot.jpa.springboot_jpa.entities.Person;
+import com.vasquez.springboot.jpa.springboot_jpa.repositories.PersonRepository;
+
 @SpringBootApplication
 public class SpringbootJpaApplication implements CommandLineRunner {
+
+	@Autowired
+	private PersonRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootJpaApplication.class, args);
@@ -13,8 +22,9 @@ public class SpringbootJpaApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'run'");
+		List<Person> persons = (List<Person>) repository.findAll();
+		persons.forEach(p-> System.out.println(p));
+
 	}
 
 }
